44b01ac38a9b0bbab1e24e11e6a3aabf1508a5f0
b00gyv00gy <43641294+b00gyv00gy@users.noreply.github.com>
2021-11-15 16:56:12 +0300

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r(mt^czz1hl+83apzz=gga((yd6i7597qubv6g##qq#1tco)gu'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
